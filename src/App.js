14cfbb2be3ad4dcaf87e3d9e425881485a501488
PavelTolopilo <pavelTolopilo@coherentsolutions.com>
2022-09-04 11:22:47 +0300

    Added list of products

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products, setProducts] = useState(null);
+
+    useEffect(() => {
+        if (!products) {
+            fetch('https://fakestoreapi.com/products')
+                .then(res => res.json())
+                .then(json => setProducts(json))
+        }
+    });
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>TA CS PL</h1>
+            </header>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>category</th>
+                            <th>title</th>
+                            <th>description</th>
+                            <th>price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products?.map((item, index) =>
+                            <tr key={index}>
+                                <td>{item.id}</td>
+                                <td>{item.category}</td>
+                                <td><img src={item.image} alt={item.image} width="50%" height="50%" /></td>
+                                <td>{item.title}</td>
+                                <td>{item.description}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
 
 export default App;
