a1dc8b46d575e5cfb3da02a5bae66633c3f69e53
PavelTolopilo <pavelTolopilo@coherentsolutions.com>
2022-09-15 10:55:49 +0200

    Added OPTION request

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import './App.css';
 import {useEffect, useState} from "react";
 
 function App() {
+    // eslint-disable-next-line
     const [products, setProducts] = useState(null);
+    const productListUrl = 'https://p3anz0vr3m.execute-api.us-east-1.amazonaws.com/dev';
 
     useEffect(() => {
         if (!products) {
-            fetch('https://fakestoreapi.com/products?limit=5')
-                .then(res => res.json())
-                .then(json => setProducts(json))
+            fetch(productListUrl, {
+                'method': 'OPTIONS'
+            })
+                .then(response => {
+                    if (response.ok) {
+                        fetch(productListUrl)
+                            .then(res => res.json())
+                            .then(json => setProducts(json))
+                    }
+                })
         }
     });
+
+    const showProduct = (id) => fetch(`${productListUrl}/products/${id}`, {
+        'method': 'OPTIONS'
+    })
+        .then(res => res.json())
+        .then(json => console.log(json));
+
     return (
         <div className="App">
             <header className="App-header">
@@ -29,7 +45,7 @@ function App() {
                     </thead>
                     <tbody>
                         {products?.map((item, index) =>
-                            <tr key={index}>
+                            <tr key={index} onClick={() => showProduct(item.id)}>
                                 <td>{item.id}</td>
                                 <td>{item.category}</td>
                                 <td><img src={item.image} alt={item.image} width="50%" height="50%" /></td>
@@ -45,3 +61,7 @@ function App() {
 }
 
 export default App;
+
+// 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
+
+// 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
